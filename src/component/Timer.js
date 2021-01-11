3dd9f07258d5062b77e9087a24f9bb998e1e0b4e
shaileh Bisht <shaileshbisht765@gmail.com>
2021-01-11 19:35:54 +0530

    style hour

--- src/component/Timer.js
+++ src/component/Timer.js
@@ -20,12 +20,16 @@ function Timer() {
   return (
     <div className="timer">
       <div className="timer_left">
-        <h1>{timer.toLocaleTimeString().slice(0, 1)}</h1>
-        <h1>{timer.toLocaleTimeString().slice(2, 4)}</h1>
+        <h1 className="left_hour">{timer.toLocaleTimeString().slice(0, 1)}</h1>
+        <h1 className="left_minutes">
+          {timer.toLocaleTimeString().slice(2, 4)}
+        </h1>
       </div>
       <div className="timer_right">
-        <h1>{timer.toLocaleTimeString().slice(5, 7)}</h1>
-        <h1>{timer.toLocaleTimeString().slice(8, 11)}</h1>
+        <h1 className="right_sec">{timer.toLocaleTimeString().slice(5, 7)}</h1>
+        <h1 className="right_amPm">
+          {timer.toLocaleTimeString().slice(8, 11)}
+        </h1>
       </div>
     </div>
   );
