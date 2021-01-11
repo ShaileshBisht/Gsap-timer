1c94f226daf9b3e0d05ade124726dcd1b86bb77d
shaileh Bisht <shaileshbisht765@gmail.com>
2021-01-11 21:43:21 +0530

    add animation to time

--- src/App.js
+++ src/App.js
@@ -6,11 +6,23 @@ import gsap from "gsap";
 function App() {
   useEffect(() => {
     const t1 = gsap.timeline();
-    t1.from(".app", { duration: 1, width: "0%", opacity: 0 }).from(".card", {
-      duration: 2,
-      x: "200px",
-      opacity: 0,
-    });
+    t1.from(".app", { duration: 1, width: "0%", opacity: 0 })
+      .from(".card", {
+        duration: 2,
+        x: "200px",
+        opacity: 0,
+      })
+      .from(
+        ".gsap_ani",
+        {
+          duration: 1,
+          y: "-300px",
+          opacity: 0,
+          stagger: 0.8,
+          ease: "bounce.out(1, 0.3)",
+        },
+        "-=2"
+      );
   }, []);
 
   return (
