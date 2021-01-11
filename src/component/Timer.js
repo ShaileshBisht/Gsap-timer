019077650a98b599b02245953cb1444fc6c86e5a
shaileh Bisht <shaileshbisht765@gmail.com>
2021-01-11 17:40:35 +0530

    get hours minutes sec from Date

--- src/component/Timer.js
+++ src/component/Timer.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Timer.css";
 
 function Timer() {
+  const [date, setDate] = useState(new Date());
+  const [hour, setHour] = useState(date.getHours());
+  const [minutes, setMinutes] = useState(date.getMinutes());
+  const [sec, setSec] = useState(date.getSeconds());
+
+  console.log(hour);
+  console.log(minutes);
+  console.log(sec);
+
   return (
     <div className="timer">
-      <h1>i am timer</h1>
+      <h1>hello</h1>
     </div>
   );
 }
